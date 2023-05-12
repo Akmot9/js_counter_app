1d782afd8c23a7f3ef277164a3585da3c12cc9e1
akmot9 <95435335+Akmot9@users.noreply.github.com>
2023-05-12 14:56:59 +0200

    sub and add

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("test")
+console.log("Bonjour console")
+
+let count = 11
+
+const countAddBtn = document.getElementById("add")
+const countHeader = document.getElementById("count")
+const countSubBtn = document.getElementById("sub")
+
+countHeader.innerText = count
+countAddBtn.addEventListener("click", () => {
+    console.log("push + ")
+    count++
+    countHeader.innerText = count
+})
+
+countSubBtn.addEventListener("click", () => {
+    console.log("push - ")
+    count--
+    countHeader.innerText = count
+})
